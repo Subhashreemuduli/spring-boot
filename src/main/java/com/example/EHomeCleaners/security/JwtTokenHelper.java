089d961577dd6f5474f38d5a188209ffe92e3f6d
Subhashreemuduli <subhashreemuduli097@gmail.com>
2022-11-23 09:35:11 +0530

    Backend EHome

--- src/main/java/com/example/EHomeCleaners/security/JwtTokenHelper.java
+++ src/main/java/com/example/EHomeCleaners/security/JwtTokenHelper.java
@@ -13,9 +13,10 @@ import java.util.function.Function;
 
 @Component
 public class JwtTokenHelper {
-    public static final long JWT_TOKEN_VALIDITY=5*60*60;
+    public static final long JWT_TOKEN_VALIDITY=5*60;
 
     private String secret ="jwtTokenKey";
+
 
     public String getUsernameFromToken(String token){
          return getClaimFromToken(token, Claims::getSubject);
